68c0d0604df5db0ad210bed5102b5d2e099fd000
nerdfiles <nerdfiles@gmail.com>
2020-08-07 01:22:00 -0500

    got ajax-y

--- cli/server/index.js
+++ cli/server/index.js
@@ -1,17 +1,58 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const fs = require('fs')
 const CliCapture = require('../index.js');
 
 const app = express();
+
+
+app.use(cors());
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
 
+const baseUrl = 'http://localhost:9999'
+
 app.get('/notes', (req, res) => {
-  const notesList = fs.readFileSync('../cli/notes.json', {encoding: 'utf8', flag: 'r'});
-  res.json(JSON.parse(notesList));
+  const rawNotesList = fs.readFileSync('../cli/notes.json', {encoding: 'utf8', flag: 'r'});
+  const parsedNotesList = JSON.parse(rawNotesList);
+  const links = [
+    {
+      rel: ["self"],
+      href: [baseUrl, '/notes'].join('')
+    }
+  ];
+
+  const actions = [
+    {
+      class: "add-note",
+      href: [baseUrl, '/notes'].join(''),
+      method: 'POST',
+      fields: [
+        {
+          name: "command",
+          type: "string"
+        },
+        {
+          name: "content",
+          type: "string"
+        }
+      ]
+    }
+  ];
+
+  const responseWithControlData = {
+    class: "note",
+    ...parsedNotesList,
+    links: links,
+    actions: actions
+
+  };
+
+  res.json(responseWithControlData);
 });
 
 app.post('/notes', (req, res) => {
--- src/App.js
+++ src/App.js
@@ -6,17 +6,61 @@ class App extends Component {
 
   constructor() {
     super()
+
     this.baseUrl = 'https://donkey-bird.surge.sh/notes.json';
+    this.localUrl = 'http://localhost:9999';
+
     this.state = {
-      notesList: {}
+      notesList: {},
+      notes: {},
+      content: ''
     }
   }
 
+  extractData(data) {
+    this.setState({notes: {
+      class: data.class,
+      links: data.links,
+      actions: data.actions
+    }}, () => {
+      console.log('extract hateoas materials')
+    });
+    delete data.class;
+    delete data.links;
+    delete data.actions;
+    return data;
+  }
+
+  updateInput(e) {
+    this.setState({content: e.target.value})
+  }
+
+  async add(e) {
+    e.preventDefault()
+    const method = e.target.getAttribute('method').toLowerCase();
+
+    const response = await axios[method](e.target.href, {
+      command: 'add',
+      content: this.state.content
+    });
+
+    const configData = JSON.parse(response.config.data)
+    alert(`added note "${configData.content}"`);
+
+    // @TODO pull down the new list and componentDidUpdate?
+
+  }
+
   async componentDidMount() {
-    const response = await axios.get(this.baseUrl);
-    const notesList = response.data;
+    //const response = await axios.get(this.baseUrl);
+    const response = await axios.get([this.localUrl, '/notes'].join(''));
+    const data = this.extractData(response.data);
+
+    const notesList = data;
     this.setState({ notesList: notesList }, () => {
       console.log('Loaded notes from ' + this.baseUrl)
+      console.log('HATEOAS stuffs')
+      console.log(this.state.notes.actions)
 
     });
   }
@@ -32,6 +76,27 @@ class App extends Component {
               <li key={key}>{key} {this.state.notesList[key]}</li>
             );
           })
+        }
+        </ul>
+
+        <div>
+
+          <input onChange={(e) => this.updateInput(e)} name="content" id="content" value={this.state.content} />
+        </div>
+
+        <ul>
+        {
+          this.state.notes.actions 
+            ? (
+                <li>
+                  <a 
+                    href={this.state.notes.actions[0].href}
+                    method={this.state.notes.actions[0].method}
+                    onClick={(e) => this.add(e)}
+                  >Add</a>
+                </li>
+              )
+            : null
         }
         </ul>
       </div>
